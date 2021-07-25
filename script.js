8dab6d07a10a236b59030a4669782bc72fa85978
Vinicis Silva Barros <viniciussilvabarros05@hotmail.com>
2021-07-25 18:01:21 -0300

    Game finished

--- script.js
+++ script.js
@@ -1,107 +1,100 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const ICON = 'icon'
 
-let techs = ['bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'];
 
-let cards = null
- 
-function initializeCards(cards){
-    let gameboard = document.getElementById("gameBoard")
-    for (let card in cards){
+startGame()
+
+function startGame() {
+    game.createCardsFromTechs()
+    initializeCards(game.cards)
+}
+
+
+/*================= adicionando cards no tabuleiro============ */
+function initializeCards(cards) {
+    let gameboard = document.getElementById("GameBoard")
+    gameboard.innerHTML = ''
+    
+    game.cards.forEach((card) => {
+
         let cardElement = document.createElement('div')
-        cardElement = card.id
-        cardElement.classList.add('card')
-        cardElement.dataset.icon = card.icon
+        cardElement.id = card.id //recebendo o id randomico 
+        cardElement.classList.add('card') //formatando card
+        cardElement.dataset.icon = card.icon // setando com o icone 
 
         createCardContent(card, cardElement)
 
 
-        cardElement.addEventListener("click",flipCard)
+        cardElement.addEventListener("click", flipCard)
         gameboard.appendChild(cardElement)
-    }
+    })
 }
 
 
-function createCardContent(card, cardElement){
+function createCardContent(card, cardElement) {
     createCardFace(FRONT, card, cardElement);
-    createCardBack(BACK, card, cardElement)
+    createCardFace(BACK,card, cardElement)
 }
 
 
-function startGame(){
-    cards = createCardsFromTechs(techs)
-    shuffleCards(cards)
-    initializeCards(cards)
-}
-/*==================== Embaralhando os cards ====================== */
-function shuffleCards(cards){
-    let currentIndex = cards.lenght; /*Pegando o tamanho do array e passando como id */
-    let randomIndex= 0;
-    
-    while(currentIndex !==0){
-        randomIndex = Math.floor(Math.random()*currentIndex) /* pegando um valor randomico de acordo com 
-        o tamanho da array e aplicando um floo (pegando sempre o número anterior ex: 19.99, vai ser 19)
-        */
-        currentIndex--
+/*==================criando front do card ===================== */
+function createCardFace(face, card, element) {
 
 
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]] /*trocando as posições */
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add(face)
+    if (face === FRONT) {
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = "./imagens/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt"
     }
-}
-/*================================================================== */
 
-
-
-
-/*========================= Setando cards ========================= */
-
-function createCardsFromTechs(techs){
-    let cards = []
-
-
-    for(let tech of techs){
-        cards.push(createPairFromTech(tech)) //Setando o array cards com os elementos da array techs
-    }
-    console.log(cards.flatMap(pair => pair)) //fletMap serve para desestruturar todas as arrays que estiverem dentro de uma array maior e rotorna
-    //o parâmetro pair serve como elemento de interação semelhante ao for(let pair in cards)
+    element.appendChild(cardElementFace)
 }
 
-/*==================================================================== */
+/*==================== Criando back do card ==================== */
 
-
-
-
-
-/*============= criando atributos de identificação do card============= */
-function createPairFromTech(tech){
-    return[{
-        id: createIdWithTech(tech),
-        icon:tech,
-        flipped:false,
-    },{ id: createIdWithTech(tech),
-        icon: tech,
-        flipped:false,
-
-    }]
-}
-/*==================================================================== */
-
-
-
-
-/*=============== função para gerar um id aleatório ================== */
-function createIdWithTech(tech){
-    return  tech + parseInt(Math.random()*1000); //gerando id
-}
-createCardsFromTechs(techs)
 
 /*===================================================================== */
+
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add("flip")
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards()
+               if(game.checkGameOver()){
+                   let gameOverLayer = document.getElementById("gameOver")
+                   gameOverLayer.style.display = "flex";
+               }
+            } else {
+               setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id)
+                    let secondCardView = document.getElementById(game.secondCard.id)
+
+                    firstCardView.classList.remove('flip')
+                    secondCardView.classList.remove('flip')
+                    game.unflipCards()
+                }, 1000)
+
+            }
+        }
+        
+
+
+    }
+}
+
+
+function restart(){
+    game.clearCards()
+    startGame()
+    let gameOverLayer = document.getElementById("gameOver")
+    gameOverLayer.style.display = "none";
+}
+
